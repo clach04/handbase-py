01cd8cc1c40b3a3b4858f55dc7001770ff203756
clach04 <clach04@gmail.com>
2024-01-12 22:32:51 -0800

    Quick and dirty Unicode hack for py2 csv

--- handbase/csv/csv2db.py
+++ handbase/csv/csv2db.py
@@ -25,6 +25,8 @@ except ImportError:
 
 from db2csv import con2driver
 
+is_py3 = sys.version_info >= (3,)
+
 
 def dump_csv_to_db(csv_filename, connection_string, table_name, param_marker='?', db_driver=None, ddl_sql=None, dml_sql=None):
     """Open's named CSV file and uses header as column names.
@@ -38,12 +40,20 @@ def dump_csv_to_db(csv_filename, connection_string, table_name, param_marker='?'
     """
     db_driver = db_driver or con2driver(connection_string)
 
-    fh = codecs.open(csv_filename, 'r', encoding="cp1252")
+    #encoding = "latin1"
+    #encoding = "cp1252"
+    encoding = "utf-8"  # FIXME - I'm injecting utf8 into HanDBase BUT it does not understand it, it treats it like latin-1/15
+    if is_py3:
+        fh = codecs.open(csv_filename, 'rb', encoding=encoding)
+    else:
+        fh = open(csv_filename, 'rb')
 
     try:
         in_csv = csv.reader(fh)
         #import pdb ; pdb.set_trace()
         header = next(in_csv)
+        if not is_py3:
+            header = [x.decode('utf-8') for x in header]
         num_cols = len(header)
         print(header)
         print('*'*65)
@@ -65,7 +75,13 @@ def dump_csv_to_db(csv_filename, connection_string, table_name, param_marker='?'
         cur.execute(ddl_sql)
 
         for row in in_csv:
-            print(row)
+            if not is_py3:
+                row = [x.decode('utf-8') for x in row]
+            #print(repr(row))
+            """
+            if row[0].startswith('Power drift'):
+                import pdb ; pdb.set_trace()
+            """
             cur.execute(dml_sql, row)
         cur.close()
         con.commit()
